4fe77936a2cb855f2494b65f47a62948ec666689
Chinyere Gloria Nnanna <cgnnanna@gmail.com>
2022-06-07 09:36:39 +0100

    added solution to the prime function that checks if an integer is a prime number or not

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 // complete the function
 function prime (num) {
   // code goes here
+
+  //creates a variable to store boolean values
+  let isPrime = true;
+
+  //checks for a positive integer
+  if(num < 1){
+    isPrime = false;
+    return isPrime;
+  }
+
+  // checks for integer 1
+  if(num === 1){
+    isPrime = false;
+    return isPrime;
+  }
+
+  /*checks for a prime integer while looping through 
+  from integer 2 down to a number before that number
+  */
+  for(let i = 2; i < num; i++){
+    if(num % i === 0){
+      isPrime = false;
+      break;
+    }
+  }
+  /*
+    compares the boolean value of isPrime if it is either false or true after the loop
+  */
+  if(isPrime){
+    return true;
+  }else{
+    return false;
+  }
+
 }
 
 function solution (arg) {
