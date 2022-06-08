18c6b99d6cdfaa2c7ddd7f06b56e591b1ab6abe1
Chinyere Gloria Nnanna <cgnnanna@gmail.com>
2022-06-08 08:52:27 +0100

    optimized the function prime in index.js

--- src/index.js
+++ src/index.js
@@ -2,39 +2,15 @@
 function prime (num) {
   // code goes here
 
-  //creates a variable to store boolean value
-  let isPrime = true;
-
-  //checks if the argument is a positive integer
-  if(num < 1){
-    isPrime = false;
-    return isPrime;
-  }
-
-  // checks if the argument is 1 since 1 is neither a prime nor composite number
-  if(num === 1){
-    isPrime = false;
-    return isPrime;
-  }
-
-  /*checks for a prime integer while looping through 
-  from integer 2 down to a number before that number
-  */
-  for(let i = 2; i < num; i++){
-    if(num % i === 0){
-      isPrime = false;
-      break;
-    }
-  }
-  /*
-    compares the boolean value of isPrime if it is either false or true after the loop
-  */
-  if(isPrime){
-    return true;
-  }else{
+  if(num <= 1){
     return false;
   }
-
+  for(let i = 2; i < num; i++){
+    if(num % i === 0){
+      return false;
+    }
+  }
+  return true
 }
 
 function solution (arg) {
